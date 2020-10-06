8be250d4c3cad576168a1e4d5250ec4ad35244bf
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-10-05 22:56:51 -0500

    Update paths.js

--- paths.js
+++ paths.js
@@ -1,10 +1,8 @@
 
 // CHANGE THESE TO WORK - remember to end w/ /
 const repos = {
-    // repo1:"/path/to/something/",
-    // repo2:"/path/to/something/but/different/branch/"
-    repo1:"/Users/paul.montgomery/Desktop/maritz/working/maritz-admin-client/src/",
-    repo2:"/Users/paul.montgomery/Desktop/maritz/maritz-admin-client/src/"
+    repo1:"/path/to/something/",
+    repo2:"/path/to/something/but/different/branch/"
 }
 
 
