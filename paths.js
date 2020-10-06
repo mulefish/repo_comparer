c582bad82a2730d730405faa2905b74550ad326e
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-10-05 23:20:55 -0500

    WIP

--- paths.js
+++ paths.js
@@ -1,8 +1,10 @@
 
 // CHANGE THESE TO WORK - remember to end w/ /
 const repos = {
-    repo1:"/path/to/something/",
-    repo2:"/path/to/something/but/different/branch/"
+    // repo1:"/path/to/something/",
+    // repo2:"/path/to/something/but/different/branch/"
+    repo1:"/Users/paul.montgomery/Desktop/example_node_project/react_client/src/",
+    repo2:"/Users/paul.montgomery/Desktop/example_node_project/working/react_client/src/"
 }
 
 
