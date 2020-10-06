0dcb54a56c4afd4e7330d8de8ed5fec4d3f1181d
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-10-05 22:53:31 -0500

    2nd pass

--- paths.js
+++ paths.js
@@ -1,8 +1,11 @@
 
-// CHANGE THESE TO WORK - remember the trailing /
+// CHANGE THESE TO WORK - remember to end w/ /
 const repos = {
-    repo1:"/path/to/something/",
-    repo2:"/path/to/something/but/different/branch/"
+    // repo1:"/path/to/something/",
+    // repo2:"/path/to/something/but/different/branch/"
+    repo1:"/Users/paul.montgomery/Desktop/maritz/working/maritz-admin-client/src/",
+    repo2:"/Users/paul.montgomery/Desktop/maritz/maritz-admin-client/src/"
 }
+
 
 exports.repos = repos;
