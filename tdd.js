479a6a0e52d393ad10c17807bfc5eb806b3711db
kermitt PDX <bluefishbrowndirt@gmail.com>
2020-10-05 21:39:50 -0500

    Markdown is hard.

--- tdd.js
+++ tdd.js
@@ -2,8 +2,12 @@ const fileLogic = require('./CollectFileInformation');
 
 function test_recursive_file_reading() {
     // TODO: Get paths from a config or something
-    const newRepo = "/Users/paul.montgomery/Desktop/<REDACTED>/working/<REDACTED>-admin-client/src/"
-    const origRepo = "/Users/paul.montgomery/Desktop/<REDACTED>/<REDACTED>-admin-client/src/";
+    // const newRepo = "/Users/paul.montgomery/Desktop/<REDACTED>/working/<REDACTED>-admin-client/src/"
+    // const origRepo = "/Users/paul.montgomery/Desktop/<REDACTED>/<REDACTED>-admin-client/src/";
+
+    const newRepo = "/Users/paul.montgomery/Desktop/maritz/working/maritz-admin-client/src/"
+    const origRepo = "/Users/paul.montgomery/Desktop/maritz/maritz-admin-client/src/";
+
     const merged = fileLogic.getFiles(newRepo, origRepo);
     let count = 0; 
     for ( let directoryName in merged ) { 
